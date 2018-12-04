4c7d088dec394f41f04d40e03d4889f1ecf3e5f6
Stephen <stephenfauvergue@gmail.com>
2018-12-04 18:31:46 +0100

    add contact

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -3,9 +3,12 @@
 namespace App\Controller;
 
 
+use App\Entity\Contact;
 use App\Entity\Property;
 use App\Entity\PropertySearch;
+use App\Form\ContactType;
 use App\Form\PropertySearchType;
+use App\Notification\ContactNotification;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -27,15 +30,29 @@ class PropertyController extends AbstractController
         return $this->render('property/index.html.twig', ['properties' => $properties, 'form' => $form->createView()]);
     }
 
-    public function show($slug, $id)
+    public function show($slug, $id, Request $request, ContactNotification $notification)
     {
+
         $property = $this->getDoctrine()->getRepository(Property::class)->find($id);
+
 
         if ($property->getSlug() !== $slug) {
             return $this->redirectToRoute('property.show', ['id' => $property->getId(), 'slug' => $property->getSlug()],
                 301);
         }
 
-        return $this->render('property/show.html.twig', ['property' => $property]);
+        $contact = new Contact();
+        $contact->setProperty($property);
+        $form = $this->createForm(ContactType::class, $contact);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $notification->notify($contact);
+            $this->addFlash('success', 'Votre email a bien été envoyé');
+            return $this->redirectToRoute('property.show',
+                ['id' => $property->getId(), 'slug' => $property->getSlug()]);
+        }
+
+        return $this->render('property/show.html.twig', ['property' => $property, 'form' => $form->createView()]);
     }
 }
