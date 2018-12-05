a615b52fc32fa8ec62f1e363daf5d609508bdee4
Stephen <stephenfauvergue@gmail.com>
2018-12-05 11:18:18 +0100

    configuring Webpack Encore and add js on contactForm

---
+++ assets/js/app.js
@@ -0,0 +1,28 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+var $ = require('jquery');
+// any CSS you require will output into a single css file (app.css in this case)
+require('../css/app.css');
+
+require('select2');
+
+$('select').select2();
+
+var $contactButton = $('#contactButton');
+
+$contactButton.click(function(e) {
+    e.preventDefault();
+    $('#contactForm').slideDown();
+    $contactButton.slideUp();
+});
+
+
+// Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
+// var $ = require('jquery');
+
+console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
